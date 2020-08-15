5cf2d1c3e23c6736fb4b0656e8f09684496cbe44
sarah-johnson <sarahbeth@sarahbeth.local>
2020-08-15 14:21:43 -0700

    basic nav working

--- flask_api/api.py
+++ flask_api/api.py
@@ -8,31 +8,32 @@ from flask import Blueprint, request
 logger = logging.getLogger(__name__)
 api = Blueprint("api", __name__)
 
-@api.route("/api/list-subpages")
-def list_subpages():
-    return {"subpages": [
-        ('Rules', "👩‍⚖️"),
-        ('Character Creation', "👤"),
-        ('Species', "🐹"),
-        ('Knacks', "🤹‍♂️"),
-        ('Magic', "💫"),
-        ('Items & Loot', "🏹"),
-        ('Setting', "🏕️"),
-        ('Places', "⛰️"),
-        ('Playing as a Party', "👥"),
-    ]}
+APP_DIR = os.path.split(__file__)[0]
 
-@api.route("/api/page/<page>")
-def fetch_page_data(page):
-    app_dir = os.path.split(__file__)[0]
-    data = yaml.safe_load(open(
-        os.path.join(app_dir, 'pages', "{}.yml".format(page)), 'r'))
-    html = markdown.markdown(
-        data['content'],
-        extensions=['extra', 'smarty'],
-        output_format='html5',
-    )
-    return {
-        **data,
-        'html': html,
-    }
+@api.route("/api/page_data")
+def fetch_page_data():
+    all_pages = {}
+
+    for page_file in os.listdir(os.path.join(APP_DIR, 'pages')):
+        page_key = os.path.splitext(page_file)[0]
+        page_data = yaml.safe_load(open(os.path.join(APP_DIR, 'pages', page_file), 'r'))
+        all_pages[page_key] = page_data
+
+    for page_key in all_pages.keys():
+        all_pages[page_key]['html'] = markdown.markdown(
+            all_pages[page_key]['content'],
+            extensions=['extra', 'smarty'],
+            output_format='html5',
+        )
+        all_pages[page_key]['subpages'] = sorted([
+            {
+                'title': v['title'],
+                'icon': v['icon'],
+                'key': k,
+                'ordinal_position': v['ordinal_position'],
+            }
+            for k, v in all_pages.items()
+            if v.get('parent_page') == page_key
+        ], key=lambda x: x['ordinal_position'])
+
+    return {'pages': all_pages}
--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from "react";
 import "./App.css";
-import headerImage from './woodcuts_13.jpg';
+// import headerImage from './woodcuts_13.jpg';
 
 function PageHeader(props) {
+  const imgUrl = require("../flask_api/static/" + props.image)
   return (
     <header>
       <div className="Page-header">
-        <img src={headerImage} className="Page-header-image" alt="header image" />
+        <img src={imgUrl} className="Page-header-image" alt="header" />
       </div>
     </header>
   );
@@ -15,34 +16,118 @@ function PageHeader(props) {
 function PageIcon(props) {
   return (
     <div className="Page-header-icon">
-      <span className="Page-header-icon" role="image" aria-label={props.emoji} >
-          {props.emoji}
+      <span className="Page-header-icon">
+        {props.emoji}
       </span>
     </div>
   )
 }
 
-function LandingPage(props) {
+class ParentPageNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+
+  handlePageChange(page) {
+    this.props.onPageChange(page);
+  }
+
+  render() {
+    return (
+      <div className="ParentPageNav">
+        <a onClick={() =>
+          this.handlePageChange(this.props.parentPage)}>{this.props.parentPage}</a>
+           ->
+        <a onClick={() =>
+          this.handlePageChange(this.props.currentPage)}>{this.props.currentPage}</a>
+      </div>
+    );
+  }
+}
+
+class SubPageNav extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+
+  handlePageChange(page) {
+    this.props.onPageChange(page);
+  }
+
+  render() {
+    const subpages = this.props.subpages;
+    const listItems = subpages.map((subpage) =>
+      <li key={subpage.title}>
+        {subpage.icon} <a onClick={() =>
+          this.handlePageChange(subpage.key)}>{subpage.title}</a>
+      </li>
+    );
+    return (
+      <div className="SubPageNav">
+        <ul className="SubPageNav">{listItems}</ul>
+      </div>
+    );
+  }
+}
+
+function Page(props) {
   return (
     <div className="Page">
-      <PageHeader image="woodcuts_13.jpg" />
+      <PageHeader image={props.pageData.image} />
       <div className="Page-content">
-        <PageIcon emoji="🏞️" />
-        <h1>Karst</h1>
-        <p>A downtempo post-apocalyptic RPG with themes of adventure, exploration, and soft terror.</p>
-        <h2>Hook</h2>
-        <p>It has been a generation since the last boat arrived or returned from the now distant shores of our various homelands. Out here in the Archipelago, life has gone on. Most folk cling to island shores. Here, they form alliances with their neighbors and tend to the dying embers of civilization. Still others, restless and possessed, strike out to seek their fortune upon the waves.</p>
-        <p>You are one of these folk, out for glory, riches, power, or similar.</p>
+        <PageIcon emoji={props.pageData.icon} />
+        <ParentPageNav
+          currentPage={props.page}
+          parentPage={props.pageData.parent_page}
+          onPageChange={props.onPageChange}
+        />
+        <div dangerouslySetInnerHTML={{__html: props.pageData.html}} />
+        <SubPageNav
+          subpages={props.pageData.subpages}
+          onPageChange={props.onPageChange}
+        />
       </div>
     </div>
   );
 }
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      pageData: {},
+      currentPage: 'home',
+      currentPageData: {'subpages': [], 'image': 'woodcuts_13.jpg'},
+    }
+    this.handlePageChange = this.handlePageChange.bind(this);
+  }
+
+  handlePageChange(page) {
+    this.setState({currentPage: page});
+    this.setState({
+      currentPageData: this.state.pageData[page]
+    });
+  }
+
+  componentDidMount() {
+    fetch('/api/page_data')
+      .then(res => res.json())
+      .then(data => {
+        this.setState({pageData: data.pages});
+        this.handlePageChange('home');
+      });
+  }
+
   render() {
     return (
       <div className="App">
-        <LandingPage />
+        <Page
+          page={this.state.currentPage}
+          pageData={this.state.currentPageData}
+          onPageChange={this.handlePageChange}
+        />
       </div>
     );
   }
