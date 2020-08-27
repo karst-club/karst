6218e62eb696ca9f834d86712364a6faea5f07c6
sarah-johnson <sarahbeth@sarahbeth.local>
2020-08-27 07:32:05 -0700

    nose does not like this type hint

--- flask_api/util.py
+++ flask_api/util.py
@@ -34,7 +34,7 @@ def build_page_tree(all_pages):
     return root_node
 
 
-def build_page_data() -> dict:
+def build_page_data():
     page_data_dir = os.path.join(APP_DIR, 'static/page_data')
     all_pages = {}
     for path, _, files in os.walk(page_data_dir):
