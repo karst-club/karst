d8c272ff1dbe5949afb51161f1a37f072ad946ba
Caz (Downing-Bryant) vonKow <vonkow@users.noreply.github.com>
2020-08-30 22:53:46 -0700

    New style (#26)
    * Added a base style. Not 100% in love with it, but the teal is nice.
    
    * Added a base style. Not 100% in love with it, but the teal is nice.
    
    * Removed accidental things
    
    * moar aches

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,22 @@
 import React, { Component } from 'react';
+import styled from 'styled-components';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Page from './Page';
+
+const AppContent = styled.div`
+  font-family: 'Vollkorn', serif;
+  background: #fefef5;
+  color: #503e3a;
+  h1 {
+    color: #9a4456;
+  }
+  h2,
+  h3,
+  h4,
+  h5 {
+    color: #429c91;
+  }
+`;
 
 class App extends Component {
   constructor(props) {
@@ -27,15 +43,17 @@ class App extends Component {
     return (
       <Router>
         <div className="App">
-          <Switch>
-            <Route exact path="/">
-              <Page allPageData={this.state.pageData} />
-            </Route>
-            <Route
-              path="/:pageId"
-              children={<Page allPageData={this.state.pageData} />}
-            />
-          </Switch>
+          <AppContent>
+            <Switch>
+              <Route exact path="/">
+                <Page allPageData={this.state.pageData} />
+              </Route>
+              <Route
+                path="/:pageId"
+                children={<Page allPageData={this.state.pageData} />}
+              />
+            </Switch>
+          </AppContent>
         </div>
       </Router>
     );
