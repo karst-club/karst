0294229da889be55f3dce006f1321a229ae80c7b
Caz <caz@vonkow.com>
2021-04-22 13:28:19 -0700

    Fix type for prisma.

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,16 +1,21 @@
 import { PrismaClient } from '@prisma/client';
 
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
+}
+
 let prisma;
 
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
-  // @ts-ignore
   if (!global.prisma) {
-    // @ts-ignore
     global.prisma = new PrismaClient();
   }
-  // @ts-ignore
   prisma = global.prisma;
 }
 
